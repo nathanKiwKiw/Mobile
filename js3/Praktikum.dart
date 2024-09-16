5bd2dfd8e419ad9354d71ad006a95a557144e215
Nathan <onethanggoro7@gmail.com>
2024-09-16 14:16:55 +0700

    dart#2

--- js3/Praktikum.dart
+++ js3/Praktikum.dart
@@ -5,17 +5,14 @@ void main() {
     }
   }
 }
-
 bool isPrime(int number) {
   if (number < 2) {
     return false;
   }
-
   for (int i = 2; i <= number / 2; i++) {
     if (number % i == 0) {
       return false;
     }
   }
-
   return true;
 }

1fc2ec44b8683d161c28068fe73c13802afd5fb5
Nathan <onethanggoro7@gmail.com>
2024-09-16 14:11:27 +0700

    praktikum dart

---
+++ js3/Praktikum.dart
@@ -0,0 +1,21 @@
+void main() {
+  for (int number = 0; number <= 201; number++) {
+    if (isPrime(number)) {
+      print('Prime number: $number' + ' | Renathan | 2241720239');
+    }
+  }
+}
+
+bool isPrime(int number) {
+  if (number < 2) {
+    return false;
+  }
+
+  for (int i = 2; i <= number / 2; i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
